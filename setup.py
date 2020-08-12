8bea924895aef5990e8e19bca7a8e6dac8ef4e65
Ayan <ayam.moldagaliev159@gmail.com>
2020-08-12 11:26:48 +0600

    python lib

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name ="library1",
+    version="0.0.1",
+    description="it is just library for fun",
+    author="ayan",
+    author_email="ayan@gmail.com",
+    url="https://github.com/mazh661/python_lib",
+    license="MIT",
+    packages=["library1"],
+    install_requires=[]
+)
