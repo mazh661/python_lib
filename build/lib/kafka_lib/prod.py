980b8ff2e77832590b6927d2f1ff89e8ae4f00f2
Ayan <ayam.moldagaliev159@gmail.com>
2020-08-12 15:25:57 +0600

    asds

--- build/lib/kafka_lib/prod.py
+++ build/lib/kafka_lib/prod.py
@@ -4,7 +4,7 @@ def prod(topicName,d):
     servers=["localhost:9092"]
     topicName=topicName
     producer=KafkaProducer(bootstrap_servers=servers)
-    d ={}
+
     arrJson=json.dumps(d)
     arrBytes=bytearray(arrJson, 'utf-8')
     producer.send(topicName, arrBytes)
